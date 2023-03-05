72bf06a3a527333dc3e211eb4e81ed86377230a2
chea <christorichard83@gmail.com>
2023-03-05 20:35:27 +0700

    menambah kode di index.php, home.php, style.css

---
+++ home.php
@@ -0,0 +1,11 @@
+<div class="kotak">
+    <div class="container_home">
+        <div class="bag1">
+            <div class="text">
+                <div class="headline">Tinkercad</div>
+                <div class="gambar1"><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fnews.gallup.com%2Fpoll%2F394262%2Ffewer-bible-literal-word-god.aspx&psig=AOvVaw1wqm8BIhyRn9bL6TJ0ip_f&ust=1678109522966000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOCDvqXzxP0CFQAAAAAdAAAAABAE"></div>
+                <div class="isi1">Tinkercad merupakan sebuah platform yang merupakan web penyedia sarana bagi kita untuk belajar secara online terkait desain 3d, rangkaian elektronikam dan codeblock. Tinkercad merupakan web besutan dari Autodesk yang sudah cukup terkenal. Jika sebelum-sebelumnya Autodesk banyak memperkenalkan software-software komputer seperti software desain, animasi, kini mereka hadir dengan salah satu platform yang bernama Tinkercad. Sebenarnya, web ini sudah lama didirikan yakni pada tahun 2011. Walaupun begitu, pengembangan web ini masih berlanjut dan menjadi salah satu web yang saya rekomendasikan untuk digunakan sebagai media pembelajaran.</div>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
             minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP Programming</title>
+    <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@700&family=Lora:ital,wght@1,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
