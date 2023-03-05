b7c030540d1b45e043e1b67169956287afcadae2
chea <christorichard83@gmail.com>
2023-03-05 21:15:10 +0700

    Menambah asset dan mengubah home.php

--- home.php
+++ home.php
@@ -2,9 +2,29 @@
     <div class="container_home">
         <div class="bag1">
             <div class="text">
-                <div class="headline">Tinkercad</div>
-                <div class="gambar1"><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fnews.gallup.com%2Fpoll%2F394262%2Ffewer-bible-literal-word-god.aspx&psig=AOvVaw1wqm8BIhyRn9bL6TJ0ip_f&ust=1678109522966000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOCDvqXzxP0CFQAAAAAdAAAAABAE"></div>
-                <div class="isi1">Tinkercad merupakan sebuah platform yang merupakan web penyedia sarana bagi kita untuk belajar secara online terkait desain 3d, rangkaian elektronikam dan codeblock. Tinkercad merupakan web besutan dari Autodesk yang sudah cukup terkenal. Jika sebelum-sebelumnya Autodesk banyak memperkenalkan software-software komputer seperti software desain, animasi, kini mereka hadir dengan salah satu platform yang bernama Tinkercad. Sebenarnya, web ini sudah lama didirikan yakni pada tahun 2011. Walaupun begitu, pengembangan web ini masih berlanjut dan menjadi salah satu web yang saya rekomendasikan untuk digunakan sebagai media pembelajaran.</div>
+                <div class="headline">Evorama</div>
+                <div id="carouselExample" class="carousel slide">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="assets/img/book1.jpg" style="width:500px;height:600px;" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="assets/img/book2.jpg" style="width:500px;height:600px;" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="assets/img/book3.jpg" style="width:500px;height:600px;" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <div class="isi1">Membaca buku sangat baik sebagai latihan otak dan pikiran. Membaca dapat membantu menjaga otak agar selalu menjalankan fungsinya secara sempurna. Saat membaca, otak dituntut untuk berpikir, menganalisis berbagai masalah, mencari jalan keluar dan solusi hingga menemukan hal-hal baru.</div>
             </div>
         </div>
     </div>
--- index.php
+++ index.php
@@ -12,6 +12,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
